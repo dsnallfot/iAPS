74cf3f01813b82f9fcd7a20f29bb3533252fcda8
dsnallfot <72826201+dsnallfot@users.noreply.github.com>
2023-07-14 15:36:51 +0200

    Update TagCloudView.swift
    sv_SE  begrepp->färger

--- FreeAPS/Sources/Views/TagCloudView.swift
+++ FreeAPS/Sources/Views/TagCloudView.swift
@@ -60,12 +60,15 @@ struct TagCloudView: View {
             case textTag where textTag.contains("Bolus"):
                 return .green
             case textTag where textTag.contains("Total insulin:"),
+                 textTag where textTag.contains("Tot 24h insulin:"),
                  textTag where textTag.contains("tdd_factor"),
                  textTag where textTag.contains("Sigmoid function"),
+                 textTag where textTag.contains("Sigmoid funktion"),
                  textTag where textTag.contains("Logarithmic formula"),
                  textTag where textTag.contains("AF:"),
                  textTag where textTag.contains("Autosens/Dynamic Limit:"),
                  textTag where textTag.contains("Dynamic ISF/CR"),
+                 textTag where textTag.contains("Dynamisk ISF/CR"),
                  textTag where textTag.contains("Basal ratio"):
                 return .zt
             default:
