b26720c7395290149b226eea1f642b20224299fe
dsnallfot <72826201+dsnallfot@users.noreply.github.com>
2023-08-11 09:18:23 +0200

    Update Statistics.swift
    Display current oref0 threshold when bolus alert about dropping below threshold

--- FreeAPS/Sources/Models/Statistics.swift
+++ FreeAPS/Sources/Models/Statistics.swift
@@ -1,211 +1,115 @@
 import Foundation
 
-struct Statistics: JSON, Equatable {
-    var created_at: Date
-    var iPhone: String
-    var iOS: String
-    var Build_Version: String
-    var Build_Number: String
-    var Branch: String
-    var CopyRightNotice: String
-    var Build_Date: Date
-    var Algorithm: String
-    var AdjustmentFactor: Decimal
-    var Pump: String
-    var CGM: String
-    var insulinType: String
-    var peakActivityTime: Decimal
-    var Carbs_24h: Decimal
-    var GlucoseStorage_Days: Decimal
-    var Statistics: Stats
-
-    init(
-        created_at: Date,
-        iPhone: String,
-        iOS: String,
-        Build_Version: String,
-        Build_Number: String,
-        Branch: String,
-        CopyRightNotice: String,
-        Build_Date: Date,
-        Algorithm: String,
-        AdjustmentFactor: Decimal,
-        Pump: String,
-        CGM: String,
-        insulinType: String,
-        peakActivityTime: Decimal,
-        Carbs_24h: Decimal,
-        GlucoseStorage_Days: Decimal,
-        Statistics: Stats
-    ) {
-        self.created_at = created_at
-        self.iPhone = iPhone
-        self.iOS = iOS
-        self.Build_Version = Build_Version
-        self.Build_Number = Build_Number
-        self.Branch = Branch
-        self.CopyRightNotice = CopyRightNotice
-        self.Build_Date = Build_Date
-        self.Algorithm = Algorithm
-        self.AdjustmentFactor = AdjustmentFactor
-        self.Pump = Pump
-        self.CGM = CGM
-        self.insulinType = insulinType
-        self.peakActivityTime = peakActivityTime
-        self.Carbs_24h = Carbs_24h
-        self.GlucoseStorage_Days = GlucoseStorage_Days
-        self.Statistics = Statistics
-    }
-
-    static func == (lhs: Statistics, rhs: Statistics) -> Bool {
-        lhs.created_at == rhs.created_at
-    }
-
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(created_at)
-    }
+struct Suggestion: JSON, Equatable {
+    let reason: String
+    let units: Decimal?
+    let insulinReq: Decimal?
+    let eventualBG: Int?
+    let sensitivityRatio: Decimal?
+    let rate: Decimal?
+    let duration: Int?
+    let iob: Decimal?
+    let cob: Decimal?
+    var predictions: Predictions?
+    let deliverAt: Date?
+    let carbsReq: Decimal?
+    let temp: TempType?
+    let bg: Decimal?
+    let reservoir: Decimal?
+    let isf: Decimal?
+    var timestamp: Date?
+    var recieved: Bool?
+    let tdd: Decimal?
+    let insulin: Insulin?
+    let current_target: Decimal?
+    let insulinForManualBolus: Decimal?
+    let manualBolusErrorString: Decimal?
+    let minDelta: Decimal?
+    let expectedDelta: Decimal?
+    let minGuardBG: Decimal?
+    let minPredBG: Decimal?
+    let threshold: Decimal?
 }
 
-extension Statistics {
-    private enum CodingKeys: String, CodingKey {
-        case created_at
-        case iPhone
-        case iOS
-        case Build_Version
-        case Build_Number
-        case Branch
-        case CopyRightNotice
-        case Build_Date
-        case Algorithm
-        case AdjustmentFactor
-        case Pump
-        case CGM
-        case insulinType
-        case peakActivityTime
-        case Carbs_24h
-        case GlucoseStorage_Days
-        case Statistics
-    }
+struct Predictions: JSON, Equatable {
+    let iob: [Int]?
+    let zt: [Int]?
+    let cob: [Int]?
+    let uam: [Int]?
 }
 
-struct LoopCycles: JSON, Equatable {
-    var loops: Int
-    var errors: Int
-    var readings: Int
-    var success_rate: Decimal
-    var avg_interval: Decimal
-    var median_interval: Decimal
-    var min_interval: Decimal
-    var max_interval: Decimal
-    var avg_duration: Decimal
-    var median_duration: Decimal
-    var min_duration: Decimal
-    var max_duration: Decimal
-}
-
-struct Averages: JSON, Equatable {
-    var Average: Durations
-    var Median: Durations
-}
-
-struct Durations: JSON, Equatable {
-    var day: Decimal
-    var week: Decimal
-    var month: Decimal
-    var total: Decimal
-}
-
-struct Threshold: JSON, Equatable {
-    var low: Decimal
-    var high: Decimal
-}
-
-struct TIRs: JSON, Equatable {
-    var TIR: Durations
-    var Hypos: Durations
-    var Hypers: Durations
-    var Threshold: Threshold
-}
-
-struct Ins: JSON, Equatable {
+struct Insulin: JSON, Equatable {
     let TDD: Decimal?
     let bolus: Decimal?
     let temp_basal: Decimal?
     let scheduled_basal: Decimal?
-    let total_average: Decimal?
 }
 
-struct Variance: JSON, Equatable {
-    var SD: Durations
-    var CV: Durations
-}
-
-struct Stats: JSON, Equatable {
-    var Distribution: TIRs
-    var Glucose: Averages
-    var HbA1c: Durations
-    var LoopCycles: LoopCycles
-    var Insulin: Ins
-    var Variance: Variance
-}
-
-extension LoopCycles {
+extension Suggestion {
     private enum CodingKeys: String, CodingKey {
-        case loops
-        case errors
-        case readings
-        case success_rate
-        case avg_interval
-        case median_interval
-        case min_interval
-        case max_interval
-        case avg_duration
-        case median_duration
-        case min_duration
-        case max_duration
+        case reason
+        case units
+        case insulinReq
+        case eventualBG
+        case sensitivityRatio
+        case rate
+        case duration
+        case iob = "IOB"
+        case cob = "COB"
+        case predictions = "predBGs"
+        case deliverAt
+        case carbsReq
+        case temp
+        case bg
+        case reservoir
+        case timestamp
+        case recieved
+        case isf = "ISF"
+        case tdd = "TDD"
+        case insulin
+        case current_target
+        case insulinForManualBolus
+        case manualBolusErrorString
+        case minDelta
+        case expectedDelta
+        case minGuardBG
+        case minPredBG
+        case threshold
     }
 }
 
-extension Averages {
+extension Predictions {
     private enum CodingKeys: String, CodingKey {
-        case Average
-        case Median
+        case iob = "IOB"
+        case zt = "ZT"
+        case cob = "COB"
+        case uam = "UAM"
     }
 }
 
-extension TIRs {
-    private enum CodingKeys: String, CodingKey {
-        case TIR
-        case Hypos
-        case Hypers
-        case Threshold
-    }
-}
-
-extension Ins {
+extension Insulin {
     private enum CodingKeys: String, CodingKey {
         case TDD
         case bolus
         case temp_basal
         case scheduled_basal
-        case total_average
     }
 }
 
-extension Variance {
-    private enum CodingKeys: String, CodingKey {
-        case SD
-        case CV
-    }
+protocol SuggestionObserver {
+    func suggestionDidUpdate(_ suggestion: Suggestion)
 }
 
-extension Stats {
-    private enum CodingKeys: String, CodingKey {
-        case Distribution
-        case Glucose
-        case HbA1c
-        case LoopCycles
-        case Insulin
-        case Variance
+protocol EnactedSuggestionObserver {
+    func enactedSuggestionDidUpdate(_ suggestion: Suggestion)
+}
+
+extension Suggestion {
+    var reasonParts: [String] {
+        reason.components(separatedBy: "; ").first?.components(separatedBy: ", ") ?? []
+    }
+
+    var reasonConclusion: String {
+        reason.components(separatedBy: "; ").last ?? ""
     }
 }
