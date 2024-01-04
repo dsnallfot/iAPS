ea8314b4f3b90f509272053506c74ce755da8706
Joe Moran <mojo@moranfoundation.org>
2024-01-04 11:52:04 -0800

    Fix Silence Pod toggle bug which inadvertently cleared configured alerts (#452)
    (cherry picked from commit 13195a3a95342bd126a34cce503e3f14d11075b1)

--- Dependencies/OmniBLE/OmniBLE/PumpManager/PodCommsSession.swift
+++ Dependencies/OmniBLE/OmniBLE/PumpManager/PodCommsSession.swift
@@ -382,8 +382,8 @@ public class PodCommsSession {
     }
 
     // Configures the given pod alert(s) and registers the newly configured alert slot(s).
-        // When re-configuring all the pod alerts for a silence pod toggle, the optional acknowledgeAll can be
-        // specified to first acknowledge and clear all possible pending pod alerts and pod alert configurations.
+    // When re-configuring all the pod alerts for a silence pod toggle, the optional acknowledgeAll can be
+    // specified to first acknowledge and clear all possible pending pod alerts and pod alert configurations.
     @discardableResult
     func configureAlerts(_ alerts: [PodAlert], acknowledgeAll: Bool = false, beepBlock: MessageBlock? = nil) throws -> StatusResponse {
         let configurations = alerts.map { $0.configuration }
@@ -391,10 +391,10 @@ public class PodCommsSession {
         let blocksToSend: [MessageBlock]
         if acknowledgeAll {
             // Do the acknowledgeAllAlerts command first to clear all previous pod alert configurations.
-                        let acknowledgeAllAlerts = AcknowledgeAlertCommand(nonce: podState.currentNonce, alerts: AlertSet(rawValue: ~0))
-                        blocksToSend = [acknowledgeAllAlerts, configureAlerts]
-                    } else {
-                        blocksToSend = [configureAlerts]
+            let acknowledgeAllAlerts = AcknowledgeAlertCommand(nonce: podState.currentNonce, alerts: AlertSet(rawValue: ~0))
+            blocksToSend = [acknowledgeAllAlerts, configureAlerts]
+        } else {
+            blocksToSend = [configureAlerts]
         }
         let status: StatusResponse = try send(blocksToSend, beepBlock: beepBlock)
         for alert in alerts {
