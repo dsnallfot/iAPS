d755db61b94e420ac0766ec32cc3a2e9acb05e90
dsnallfot <72826201+dsnallfot@users.noreply.github.com>
2024-02-10 16:10:31 +0100

    Testa trigga uppladdning av både TT och Carbs om ngn av dem uppdateras (ibland missas uppladdning till ns om man både aktiverar tt och reggar kh samtidigt)

--- FreeAPS/Sources/Services/Network/NightscoutManager.swift
+++ FreeAPS/Sources/Services/Network/NightscoutManager.swift
@@ -790,12 +790,14 @@ extension BaseNightscoutManager: PumpHistoryObserver {
 extension BaseNightscoutManager: CarbsObserver {
     func carbsDidUpdate(_: [CarbsEntry]) {
         uploadCarbs()
+        uploadTempTargets() //Daniel: Testa trigga uppladdning av både TT och Carbs om ngn av dem uppdateras (ibland missas uppladdning till ns om man både aktiverar tt och reggar kh samtidigt)
     }
 }
 
 extension BaseNightscoutManager: TempTargetsObserver {
     func tempTargetsDidUpdate(_: [TempTarget]) {
         uploadTempTargets()
+        uploadCarbs() //Daniel: Testa trigga uppladdning av både TT och Carbs om ngn av dem uppdateras (ibland missas uppladdning till ns om man både aktiverar tt och reggar kh samtidigt)
     }
 }
 
