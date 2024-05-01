23397b430e37fbe36689a845ad32563d8e91b809
dsnallfot <72826201+dsnallfot@users.noreply.github.com>
2024-05-01 12:00:11 +0200

    Comment out all fetching of treatments and announcements from NS since no longer needed

--- FreeAPS/Sources/APS/FetchTreatmentsManager.swift
+++ FreeAPS/Sources/APS/FetchTreatmentsManager.swift
@@ -3,45 +3,49 @@ import Foundation
 import SwiftDate
 import Swinject
 
-protocol FetchTreatmentsManager {}
+// Comment out all code to deactivate featchtreatments
 
-final class BaseFetchTreatmentsManager: FetchTreatmentsManager, Injectable {
-    private let processQueue = DispatchQueue(label: "BaseFetchTreatmentsManager.processQueue")
-    @Injected() var nightscoutManager: NightscoutManager!
-    @Injected() var tempTargetsStorage: TempTargetsStorage!
-    @Injected() var carbsStorage: CarbsStorage!
+/*
+ protocol FetchTreatmentsManager {}
 
-    private var lifetime = Lifetime()
-    private let timer = DispatchTimer(timeInterval: 1.minutes.timeInterval)
+ final class BaseFetchTreatmentsManager: FetchTreatmentsManager, Injectable {
+     private let processQueue = DispatchQueue(label: "BaseFetchTreatmentsManager.processQueue")
+     @Injected() var nightscoutManager: NightscoutManager!
+     @Injected() var tempTargetsStorage: TempTargetsStorage!
+     @Injected() var carbsStorage: CarbsStorage!
 
-    init(resolver: Resolver) {
-        injectServices(resolver)
-        subscribe()
-    }
+     private var lifetime = Lifetime()
+     private let timer = DispatchTimer(timeInterval: 1.minutes.timeInterval)
 
-    private func subscribe() {
-        timer.publisher
-            .receive(on: processQueue)
-            .flatMap { _ -> AnyPublisher<([CarbsEntry], [TempTarget]), Never> in
-                debug(.nightscout, "FetchTreatmentsManager heartbeat")
-                debug(.nightscout, "Start fetching carbs and temptargets")
-                return Publishers.CombineLatest(
-                    self.nightscoutManager.fetchCarbs(),
-                    self.nightscoutManager.fetchTempTargets()
-                ).eraseToAnyPublisher()
-            }
-            .sink { carbs, targets in
-                let filteredCarbs = carbs.filter { !($0.enteredBy?.contains(CarbsEntry.manual) ?? false) }
-                if filteredCarbs.isNotEmpty {
-                    self.carbsStorage.storeCarbs(filteredCarbs)
-                }
-                let filteredTargets = targets.filter { !($0.enteredBy?.contains(TempTarget.manual) ?? false) }
-                if filteredTargets.isNotEmpty {
-                    self.tempTargetsStorage.storeTempTargets(filteredTargets)
-                }
-            }
-            .store(in: &lifetime)
-        timer.fire()
-        timer.resume()
-    }
-}
+     init(resolver: Resolver) {
+         injectServices(resolver)
+         subscribe()
+     }
+
+     private func subscribe() {
+         timer.publisher
+             .receive(on: processQueue)
+             .flatMap { _ -> AnyPublisher<([CarbsEntry], [TempTarget]), Never> in
+                 debug(.nightscout, "FetchTreatmentsManager heartbeat")
+                 debug(.nightscout, "Start fetching carbs and temptargets")
+                 return Publishers.CombineLatest(
+                     self.nightscoutManager.fetchCarbs(),
+                     self.nightscoutManager.fetchTempTargets()
+                 ).eraseToAnyPublisher()
+             }
+             .sink { carbs, targets in
+                 let filteredCarbs = carbs.filter { !($0.enteredBy?.contains(CarbsEntry.manual) ?? false) }
+                 if filteredCarbs.isNotEmpty {
+                     self.carbsStorage.storeCarbs(filteredCarbs)
+                 }
+                 let filteredTargets = targets.filter { !($0.enteredBy?.contains(TempTarget.manual) ?? false) }
+                 if filteredTargets.isNotEmpty {
+                     self.tempTargetsStorage.storeTempTargets(filteredTargets)
+                 }
+             }
+             .store(in: &lifetime)
+         timer.fire()
+         timer.resume()
+     }
+ }*/
+// Comment out all code to deactivate featchtreatments
